dced5771af8bc39d1fdb87cec04c94ad9c0546dc
chun-sung <springstar@daum.net>
2019-01-08 15:11:00 +0900

    Icon

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,12 +1,105 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react';
+import styled from 'styled-components'
+import ManImage from '../data/img/ManImage.png'
+import GirlImage from '../data/img/GirlImage.png'
+import CustomSample from '../data/img/CustomSample.png'
 
 export default class Avatar extends Component {
-  render() {
-    
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+ 
+    render() {
+        // console.log(this.props);
+        return (
+            <div>                
+                <Persons />
+            </div>
+        );
+    }
 }
+
+const Persons = () => {
+   
+    return (
+        <Fragment>
+            <PersonCircle />
+            <PersonSquare />
+            <PersonCustom  /> 
+            <PersonCircleSmall />           
+            <PersonSquareSmall />
+            <PersonCircleVerySmall />
+            <PersonSquareVerySmall />
+        </Fragment>
+    );
+}
+
+// 원형 기본 아바타
+export const PersonCircle = styled.div`
+  margin: 5px;
+  width: 100px;
+  height: 100px;  
+  border-radius: 50%;
+  cursor: pointer;
+  background-image: url(${ManImage});
+  &hover {
+  }  
+`;
+
+// 사각형 기본 아바타
+export const PersonSquare = styled.div`
+  margin: 5px;
+  width: 100px;
+  height: 100px;  
+  cursor: pointer;
+  background-image: url(${GirlImage});
+`;
+
+// 사용자 아바타
+export const PersonCustom = styled.div`
+  display: none;
+  margin: 5px;
+  width: 100px;
+  height: 100px;  
+  cursor: pointer;
+  background-image: url(${CustomSample});
+`;
+
+// 작은 원형 아바타
+export const PersonCircleSmall = styled.div`
+  margin: 5px;
+  width: 50px;
+  height: 50px;  
+  border-radius: 50%;
+  cursor: pointer;
+  background-image: url(${ManImage});
+  background-size: cover;
+`;
+
+// 작은 사각형 아바타
+export const PersonSquareSmall = styled.div`
+  margin: 5px;
+  width: 50px;
+  height: 50px;  
+  cursor: pointer;
+  background-image: url(${GirlImage});
+  background-size: cover;
+`;
+
+// 매우 작은 원형 아바타
+export const PersonCircleVerySmall = styled.div`
+  margin: 5px;
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  cursor: pointer;
+  background-image: url(${ManImage});
+  background-size: cover;
+`;
+
+// 매우 작은 사각형 아바타
+export const PersonSquareVerySmall = styled.div`
+  margin: 5px;
+  width: 25px;
+  height: 25px;  
+  cursor: pointer;
+  background-image: url(${GirlImage});
+  background-size: cover;
+`;
--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,11 +1,93 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react';
+import styled, { createGlobalStyle } from 'styled-components'
+import Main from "react-icons/lib/md/subtitles";
+import Person from "react-icons/lib/md/group";
+import Search from "react-icons/lib/go/search";
+import Notifications from "react-icons/lib/io/android-notifications";
+import FullMenu from "react-icons/lib/md/format-align-justify";
+import Messenger from "react-icons/lib/fa/commenting";
+import RecommendPerson from "react-icons/lib/ti/user-add";
+import FriednCode from "react-icons/lib/io/ios-grid-view-outline";
 
-export default class Icon extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+const defaultColor = "rgba(65, 95, 160, 9)";
+const ClickedColor = "rgba(52, 61, 108, 9)";
+
+export default class Icons extends Component {
+    state = {
+        color: defaultColor,               
+    }        
+    
+    handleClick = (e) => {
+        e.stopPropagation();      
+        this.setState({
+            color: ClickedColor
+
+        })             
+    }    
+
+    render() {        
+        return <div>
+            <GlobalStyle />
+            <IconContainer color={this.state.color} onClick={this.handleClick} />
+          </div>        
+    }
 }
+
+const IconContainer = (props) => {     
+    return (
+        <Fragment>
+            <Icon color={props.color} onClick={props.onClick}>
+                <h2><Main  /></h2>
+            </Icon>
+            <Icon color={props.color} onClick={props.onClick}>
+                <h2><Person /></h2>
+            </Icon>
+            <Icon color={props.color} onClick={props.onClick}>
+                <h2><Messenger /></h2>
+            </Icon >
+            <Icon color={props.color} onClick={props.onClick}>
+                <h2><Notifications onClick={props.onClick}/></h2>
+            </Icon>
+            <Icon color={props.color} onClick={props.onClick}>
+                <h2><Search /></h2>
+            </Icon>
+            <Icon color={props.color} onClick={props.onClick}>
+                <h2><FullMenu /></h2>
+            </Icon>                    
+            
+            
+            <RecommendPerson />                       
+            <FriednCode />           
+            <Search />
+        </Fragment>
+    );
+};
+const GlobalStyle = createGlobalStyle`
+    body{
+        margin: 0px;
+        margin-right: -2px;          
+    }
+    h2 {
+        margin: 0px;
+    }
+`;
+
+const Icon = styled.div`
+    display: inline-block;
+    text-align: center;
+    margin: 0 auto;
+    width: 16.65%;
+    height: 25px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    line-height: 0px         
+    background-color:${props => props.color};    
+    cursor: pointer;
+    
+    `;
+    
+    // &: hover {
+    //   background - color: rgba(65, 91, 140, 9);
+    //   background - color: rgba(52, 61, 108, 9);
+    //   color: white;
+    // }   
